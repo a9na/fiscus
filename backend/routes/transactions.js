0b17e81be307939ca88fca31bcc173cada220213
a9na <anaiscoding@gmail.com>
2024-05-09 17:38:22 +0200

    commit

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,9 +1,11 @@
-const { addIncome } = require('../controllers/income.');
 
 const router = require('express').Router();
 
 router.post('/add-income', addIncome)
         .get('/get-incomes', getIncomes)
         .delete('/delete-income/:id', deleteIncome)
+        .post('/add-expenses', addExpenses)
+        .get('/get-expenses', getExpenses)
+        .delete('/delete-expenses/:id', deleteExpenses)
 
 module.exports = router
