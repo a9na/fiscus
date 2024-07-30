8eeac30d9d3e577af1318fabb9ec8245eea85845
a9na <anaiscoding@gmail.com>
2024-07-30 11:44:09 +0200

    commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import { GlobalStyle } from './styles/GlobalStyle';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Styling />
+    <GlobalStyle />
     <GlobalProvider>
       <App />
     </GlobalProvider>
